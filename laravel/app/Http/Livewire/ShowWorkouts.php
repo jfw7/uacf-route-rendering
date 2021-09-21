19a7d358c453101a684381abfaa859ee74fe8f2b
jfw7 <jwall@peopledesigns.com>
2021-09-20 23:22:27 -0400

    prepare for gpx

--- laravel/app/Http/Livewire/ShowWorkouts.php
+++ laravel/app/Http/Livewire/ShowWorkouts.php
@@ -3,20 +3,31 @@
 namespace App\Http\Livewire;
 
 use Http;
+
 use Livewire\Component;
+use phpGPX\phpGPX;
 
 class ShowWorkouts extends Component
 {
+    protected $gpx;
+    protected $user;
+
+    public function mount()
+    {
+        $this->gpx = new phpGpx;
+        $this->user = session('user');
+    }
+
     public function render()
     {
-        $user = session('user');
-        $workouts = Http::withHeaders([
-            'Authorization' => "Bearer {$user->token}",
-        ])->get('https://api.ua.com/v7.1/workout/6104325667', ['user' => $user->id, 'order_by' => '-start_datetime', 'field_set' => 'time_series'])->object();
-        $activity = Http::withHeaders([
-            'Authorization' => "Bearer {$user->token}",
-        ])->get('https://api.ua.com/v7.1/activity_story', ['feed_type' => 'user', 'feed_id' => $user->id])->object();
+        return view('livewire.show-workouts');
+    }
 
-        return view('livewire.show-workouts', ['workouts' => $workouts]);
+    public function getRoutesProperty()
+    {
+        $stories = collect(Http::withHeaders([
+            'Authorization' => "Bearer {$this->user->token}",
+        ])->get('https://api.ua.com/v7.1/activity_story', ['token' => '3d5dde80-0f13-11ec-8080-8001177b026b', 'feed_type' => 'user', 'feed_id' => $this->user->id, 'feed_view' => 'me'])->object()); //->_embedded->activity_stories);
+        return $stories;
     }
 }
--- laravel/resources/views/livewire/show-workouts.blade.php
+++ laravel/resources/views/livewire/show-workouts.blade.php
@@ -1,7 +1,20 @@
-<div>
-    @dd($workouts->time_series->position)
-    @foreach($workouts->_embedded->workouts as $workout)
-        @dump($workout)
-        <hr>
-    @endforeach
+<div
+    x-data="{
+        observe () {
+            let observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        @this.call('loadMore')
+                    }
+                })
+            }, {
+                root: null
+            })
+
+            observer.observe(this.$el)
+        }
+    }"
+    x-init="observe"
+>
+    @dump($this->routes)
 </div>
